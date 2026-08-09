76450e934c65906b6b1bac22a79df12ba26d3a96
technetium <github@technetium.be>
2026-08-09 20:17:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ except KeyError:
 if __name__ == "__main__":
     logger.info(f"Token value: {SOME_SECRET}")
 
+    # https://httpbin.org/headers
     r = requests.get('https://weather.talkpython.fm/api/weather/?city=Berlin&country=DE')
     if r.status_code == 200:
         data = r.json()
